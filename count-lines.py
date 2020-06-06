e2c063f0846599bc281cc8ce80599a7c36d77be8
R R <rebecca.ruthven@gmail.com>
2020-06-06 16:50:02 -0700

    Count lines in file and flag empty lines inside curly brackets.

--- count-lines.py
+++ count-lines.py
@@ -1,7 +1,24 @@
 ## Open a hard-coded file and return the number of lines in the file.
+filename = "data\sample7sunny_root.json"
+f = open(file=filename)
+allLines = f.readlines()
+firstLine = False
+lastLine = False
+textLength = 0
+for x in allLines:
+    if x == '{\n':
+        firstLine = True
+    elif x == '}\n':
+        lastLine = True
+    elif firstLine == True and lastLine == False:
+        textLength += 1
+        if not x.strip():
+            print("Space in the middle!")
+    # print(textLength)
 
-f = open(file="data\sample7sunny_root.json")
+print(f"File '{filename}' is {len(allLines)} lines long,")
+print(f"and there are {textLength} lines of text.")
 
-TotalLines = len(f.readlines())
+## Figure out whether the first and last lines are empty.
 
-print(f"Your file is {TotalLines} lines long.")
+## Spit out all the segment numbers in the file
