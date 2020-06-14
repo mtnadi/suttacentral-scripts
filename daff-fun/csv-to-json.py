c2d4595cf65148641f667ac856b7f1f5cefc8c62
tracylau <tracylau@users.noreply.github.com>
2020-06-13 23:07:35 -0700

    Fix daff scripts to actually handle optional args and play nice with openoffice (#2)

--- daff-fun/csv-to-json.py
+++ daff-fun/csv-to-json.py
@@ -2,25 +2,27 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Convert a csv'd json bilara-data file back into json")
+parser = argparse.ArgumentParser(description="Convert a csv'd json bilara-data file back into json.  The csv if edited in OpenOffice should be saved with Character set UTF-8, Field delimiter {Tab}, and Quote all text cells unchecked")
 parser.add_argument("filename", help="The csv file to convert")
-parser.add_argument("--dest", help="Specify destination filename")
-parser.add_argument("-v", help="Print each line of new file")
+parser.add_argument("--dest", action="store", dest="dest", help="Specify destination filename")
+parser.add_argument("-v", action="store_true", help="Print each line of new file")
 args = parser.parse_args()
 
 csv_filename = args.filename
 if csv_filename[-4:] != ".csv":
     raise ValueError("Invalid file")
 
-json_filename = csv_filename.split(".")[0] + "-new.json"
+json_filename = args.dest if args.dest else csv_filename[:-3] + "new.json"
 print(f"creating {json_filename}...")
 
 with open(json_filename, "w") as f:
     f.write("{\n")
     with open(csv_filename) as c:
         for line in c:
-            [x, y] = line.strip().split(",", 1)
+            [x, y] = line.strip("\n").split("\t")
+            out_line = f'  "{x}": "{y}",\n'
             if args.v:
-                print("  " + x + ": " + y + "\n")
-            f.write("  " + x + ": " + y + "\n")
-    f.write("}\n")
+                print(out_line)
+            f.write(out_line)
+    f.write("}")
+    print("TODO: remove trailing comma from last line")
