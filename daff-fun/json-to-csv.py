90084151d005a9a61eef662bc08369e0232f5a7a
Tracy Lau <tracylau@telus.net>
2020-06-13 23:05:27 -0700

    Fix daff scripts to actually handle optional args and play nice with openoffice

--- daff-fun/json-to-csv.py
+++ daff-fun/json-to-csv.py
@@ -4,22 +4,22 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Convert a json bilara-data file into csv")
 parser.add_argument("filename", help="The json file to convert")
-parser.add_argument("--dest", help="Specify destination filename")
-parser.add_argument("-v", help="Print each line of new file")
+parser.add_argument("--dest", action="store", dest="dest", help="Specify destination filename")
+parser.add_argument("-v", action="store_true", help="Print each line of new file")
 args = parser.parse_args()
 
 json_filename = args.filename
 if json_filename[-5:] != ".json":
     raise ValueError("Invalid file")
 
-csv_filename = json_filename.split(".")[0] + ".csv"
+csv_filename = args.dest if args.dest else json_filename[:-4] + "csv"
 print(f"creating {csv_filename}...")
 
 with open(csv_filename, "w") as c:
     with open(json_filename) as f:
         for line in f:
             if line.strip() not in ["{", "}"]:
-                [x, y] = line.strip().split(": ", 1)
+                [x, y] = line.strip().rstrip(",").split(": ", 1)
                 if args.v:
                     print(x + "," + y + "\n")
                 c.write(x + "," + y + "\n")
