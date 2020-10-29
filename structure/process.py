53942289ad030c5dee25a45c39fe661f8d6cdc23
Tracy Lau <tracylau@telus.net>
2020-10-28 21:37:00 -0700

    Remove hacks and ignoring of sutta .jsons

--- structure/process.py
+++ structure/process.py
@@ -20,7 +20,7 @@ def tree_filename(filename, debug=False):
         return "./tree-sandbox.json"
     return new_filename(filename, "tree")
 
-def split_file(filename, debug=False):
+def split_file(filename, all_names, debug=False):
     # Read in .json file.
     with open(filename) as f:
         data = json.load(f)
@@ -29,12 +29,13 @@ def split_file(filename, debug=False):
     tree = OrderedDict()
     names = OrderedDict()
 
-    # Cheat.  Go over all the nodes and record whether it is the parent of a leaf node.
+    # Cheat.  Go over all the nodes and record whether it is the parent of a
+    # leaf node.
     previous_entry = None
     for entry in data:
         if "type" in entry and entry["type"] != "text":
             entry["grandparent"] = True
-        else:
+        elif previous_entry:
             if previous_entry["grandparent"]:
                 previous_entry["grandparent"] = False
             entry["grandparent"] = False
@@ -51,17 +52,13 @@ def split_file(filename, debug=False):
         # To create the tree, we need to split up the path into bits, and then make
         # a tree.
         split_path = entry["_path"].split("/")
-        # Just to get past gdhp.json, pdhp.json
-#        if split_path[0] == '':
-#            split_path = split_path[1:]
-        # END HACK
+
+        # First check uniqueness of last element of the path.
+        all_names.setdefault(split_path[-1], []).append(filename)
+
         # Traverse the tree until we are at the point where we insert this entry.
         parent = tree
         for i in range(len(split_path) - 1):
-            # START HACK for minor-lzh, other-san, other-xct
-#            if split_path[i] not in parent:
-#                parent[split_path[i]] = {}
-            # END HACK
             parent = parent[split_path[i]]
 
         print(split_path)  # TODO remove - debugging
@@ -93,29 +90,6 @@ def split_file(filename, debug=False):
 # Loop over all .json files.
 directory = "/Users/tracy/Development/sc-data/structure/division"
 ignore = [
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/kn.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas/gdhp.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas/lzh-dhp.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas/pdhp.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas/uv.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas/uvs.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/dharmapadas/xct-uv.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/minor-lzh/lzh-dharani.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/minor-lzh/lzh-ja.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/minor-lzh/lzh-nbs.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/minor-lzh/lzh-ssnp.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/minor-lzh/lzh-svk.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/arv.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/avs.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/divy.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/lal.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/mkv.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/sf.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/sht.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-san/ybs.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-xct/d.json",
-    "/Users/tracy/Development/sc-data/structure/division/sutta/other/other-xct/up.json",
     "/Users/tracy/Development/sc-data/structure/division/vinaya/lzh-dg-bi-pm.json",
     "/Users/tracy/Development/sc-data/structure/division/vinaya/lzh-dg-bu-pm-2.json",
     "/Users/tracy/Development/sc-data/structure/division/vinaya/lzh-dg-bu-pm.json",
@@ -141,6 +115,7 @@ ignore = [
     "/Users/tracy/Development/sc-data/structure/division/vinaya/san-sarv-bu-pm-tf11.json",
     "/Users/tracy/Development/sc-data/structure/division/vinaya/xct-mu-bu-pm.json",
 ]
+all_names = {} # later this can be set()
 for root, dirs, files in os.walk(directory):
     for file in files:
         filename = os.path.join(root, file)
@@ -148,4 +123,8 @@ for root, dirs, files in os.walk(directory):
             print(f"SKIPPING {filename}")
             continue
         print(f"Processing {filename}")
-        split_file(filename, debug=True)
+        split_file(filename, all_names, debug=True)
+
+for name, files in all_names.items():
+    if len(files) > 1:
+        print(f"Name {name} appears more than once.  Found in {files}")
